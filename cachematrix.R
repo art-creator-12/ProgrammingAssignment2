cb740971c59f8eacdcf6dcdb90bac92cb5993f18
art-creator-12 <54854276+art-creator-12@users.noreply.github.com>
2019-09-03 23:52:36 +0300

    task done

--- cachematrix.R
+++ cachematrix.R
@@ -3,13 +3,39 @@
 
 ## Write a short comment describing this function
 
+#For this assignment, assume that the matrix supplied is always invertible.
 makeCacheMatrix <- function(x = matrix()) {
-
+  i <- NULL
+  # matrix setter & getter
+  set <- function(y){
+    x<<-y
+    i<<-NULL
+  }
+  get <- function() x
+  # inverse matrix setter & getter
+  setinv <- function(inv) i <<- inv
+  getinv <- function() i
+  # make functions list
+  list(set = set,
+       get = get,
+       setinv = setinv,
+       getinv = getinv)
 }
 
 
 ## Write a short comment describing this function
 
 cacheSolve <- function(x, ...) {
+  i <- x$getinv()
+  #if inverse is already cached
+  if(!is.null(i)){
+    message("getting cache data")
+    return(i)
+  }
+  #if inverse is not cached
+  mx <- x$get()
+  i <- solve(mx)
+  x$setinv(i)
         ## Return a matrix that is the inverse of 'x'
+  i
 }
